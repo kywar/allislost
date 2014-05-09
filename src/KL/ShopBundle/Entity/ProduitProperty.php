214df97c6014d268c9ad1c41c7d195da1c2e16c4
pml-dev <michel.pierre012@gmail.com>
2014-05-09 11:24:55 +0200

    properties v2

--- src/KL/ShopBundle/Entity/ProduitProperty.php
+++ src/KL/ShopBundle/Entity/ProduitProperty.php
@@ -15,6 +15,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class ProduitProperty
 {
+
     /**
      * @var integer
      *
@@ -27,25 +28,63 @@ class ProduitProperty
      * })
      */
     private $produitId;
+
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="propertyId", type="integer")
+     * @var string
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="NONE")
-     * @ORM\ManyToOne(targetEntity="Property")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="propertyId", referencedColumnName="id")
-     * })
+     * @ORM\Column(name="name", type="string", length=255)
      */
-    private $propertyId;
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="format", type="string", length=255)
+     */
+    private $format;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var boolean
+     * 
+     * @ORM\Column(name="required", type="boolean")
+     */
+    private $required;
+
+    /**
+     * @var boolean
+     * 
+     * @ORM\Column(name="public", type="boolean")
+     */
+    private $public;
+
+    /**
+     * @var integer
+     * 
+     * @ORM\Column(name="ordering", type="integer", nullable=true)
+     */
+    private $ordering;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="position", type="string", length=255, nullable=true)
+     */
+    private $position;
+
     /**
      * @var string
      * 
-     * @ORM\Column(name="name", type="string", length=255)
+     * @ORM\Column(name="value", type="string", length=255)
      */
     private $value;
-
 
     /**
      * Set produitId
@@ -56,7 +95,7 @@ class ProduitProperty
     public function setProduitId($produitId)
     {
         $this->produitId = $produitId;
-    
+
         return $this;
     }
 
@@ -71,29 +110,6 @@ class ProduitProperty
     }
 
     /**
-     * Set propertyId
-     *
-     * @param integer $propertyId
-     * @return ProduitProperty
-     */
-    public function setPropertyId($propertyId)
-    {
-        $this->propertyId = $propertyId;
-    
-        return $this;
-    }
-
-    /**
-     * Get propertyId
-     *
-     * @return integer 
-     */
-    public function getPropertyId()
-    {
-        return $this->propertyId;
-    }
-
-    /**
      * Set value
      *
      * @param string $value
@@ -102,7 +118,7 @@ class ProduitProperty
     public function setValue($value)
     {
         $this->value = $value;
-    
+
         return $this;
     }
 
@@ -114,5 +130,167 @@ class ProduitProperty
     public function getValue()
     {
         return $this->value;
+    }
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return ProduitProperty
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set format
+     *
+     * @param string $format
+     * @return ProduitProperty
+     */
+    public function setFormat($format)
+    {
+        $this->format = $format;
+    
+        return $this;
+    }
+
+    /**
+     * Get format
+     *
+     * @return string 
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return ProduitProperty
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set required
+     *
+     * @param boolean $required
+     * @return ProduitProperty
+     */
+    public function setRequired($required)
+    {
+        $this->required = $required;
+    
+        return $this;
+    }
+
+    /**
+     * Get required
+     *
+     * @return boolean 
+     */
+    public function getRequired()
+    {
+        return $this->required;
+    }
+
+    /**
+     * Set public
+     *
+     * @param boolean $public
+     * @return ProduitProperty
+     */
+    public function setPublic($public)
+    {
+        $this->public = $public;
+    
+        return $this;
+    }
+
+    /**
+     * Get public
+     *
+     * @return boolean 
+     */
+    public function getPublic()
+    {
+        return $this->public;
+    }
+
+    /**
+     * Set ordering
+     *
+     * @param integer $ordering
+     * @return ProduitProperty
+     */
+    public function setOrdering($ordering)
+    {
+        $this->ordering = $ordering;
+    
+        return $this;
+    }
+
+    /**
+     * Get ordering
+     *
+     * @return integer 
+     */
+    public function getOrdering()
+    {
+        return $this->ordering;
+    }
+
+    /**
+     * Set position
+     *
+     * @param string $position
+     * @return ProduitProperty
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    
+        return $this;
+    }
+
+    /**
+     * Get position
+     *
+     * @return string 
+     */
+    public function getPosition()
+    {
+        return $this->position;
     }
 }
--- src/KL/ShopBundle/Model/ProduitModel.php
+++ src/KL/ShopBundle/Model/ProduitModel.php
@@ -22,9 +22,26 @@ abstract class ProduitModel
      */
     abstract public function getPrices();
 
+    abstract public function getProperties();
+
+    public function __call($name, $arguments)
+    {
+        if (strpos($name, "get") === 0 || strpos($name, "set") === 0) {
+            $fonctionName = substr($name, 3);
+            $assessor = substr($name, 0, 3);
+            $functionCalled = $assessor . "Value(" . $arguments . ")";
+            foreach ($this->getProperties() as $property) {
+                if ($fonctionName === $property->getName()) {
+                    return $property->$functionCalled;
+                }
+            }
+        }
+       return;
+    }
+
     public function getPrix(DateTime $date = null)
-    {        
-        if (null === $date){
+    {
+        if (null === $date) {
             $date = new DateTime();
         }
         if ($this->getPrices()->isEmpty()) {
@@ -38,7 +55,7 @@ abstract class ProduitModel
                 $last = $prix;
             }
         }
-        
+
         return $last;
     }
 
